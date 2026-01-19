297ca4a4559397c37a86fe23b5e3a21534f8a261
RPH07 <renaldyputrahasli@gmail.com>
2026-01-19 16:48:34 +0700

    feat: add Laporan component with basic layout

---
+++ mobile-app/app/(tabs)/laporan.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function Laporan() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Halaman Laporan</Text>
+    </View>
+  );
+}
