121d9ade047db5323ac73180e75c2fb5cdb8f148
RPH07 <renaldyputrahasli@gmail.com>
2026-01-19 16:48:52 +0700

    feat: add UangMasuk component for inputting cash inflow

---
+++ mobile-app/app/(tabs)/uang_masuk.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function UangMasuk() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Halaman Input Uang Masuk</Text>
+    </View>
+  );
+}
