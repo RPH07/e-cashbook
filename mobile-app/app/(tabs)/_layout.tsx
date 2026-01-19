cefb39115707f70c113a34af964a09033b32d6f0
RPH07 <renaldyputrahasli@gmail.com>
2026-01-19 16:48:13 +0700

    refactor: update TabLayout to use Ionicons and simplify tab options

--- mobile-app/app/(tabs)/_layout.tsx
+++ mobile-app/app/(tabs)/_layout.tsx
@@ -1,33 +1,49 @@
 import { Tabs } from 'expo-router';
-import React from 'react';
-
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { Ionicons } from '@expo/vector-icons';  
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
+        headerShown: false, 
+        tabBarActiveTintColor: '#1a5dab', 
+        tabBarInactiveTintColor: 'gray', 
+        tabBarStyle: {
+          height: 100,
+          paddingBottom: 10,
+          paddingTop: 10,
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: 'Beranda',
+          tabBarIcon: ({ color }) => <Ionicons name="home" size={24} color={color} />,
         }}
       />
+
       <Tabs.Screen
-        name="explore"
+        name="uang_masuk"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Uang Masuk',
+          tabBarIcon: ({ color }) => <Ionicons name="arrow-down-circle" size={24} color={color} />,
+        }}
+      />
+
+      <Tabs.Screen
+        name="uang_keluar" 
+        options={{
+          title: 'Uang Keluar',
+          tabBarIcon: ({ color }) => <Ionicons name="arrow-up-circle" size={24} color={color} />,
+        }}
+      />
+
+      <Tabs.Screen
+        name="laporan" 
+        options={{
+          title: 'Laporan',
+          tabBarIcon: ({ color }) => <Ionicons name="stats-chart" size={24} color={color} />,
         }}
       />
     </Tabs>
