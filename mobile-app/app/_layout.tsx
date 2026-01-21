330be28fe3a1e5204dedb69dd2668d6c92ad85c2
RPH07 <renaldyputrahasli@gmail.com>
2026-01-21 15:28:10 +0700

    feat: wrap Stack component with TransactionProvider in RootLayout

--- mobile-app/app/_layout.tsx
+++ mobile-app/app/_layout.tsx
@@ -1,22 +1,25 @@
 import { Stack } from "expo-router";
+import { TransactionProvider } from "@/context/TransactionContext";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{ headerShown: false }}
-      />
-      
-      <Stack.Screen
-        name="login"
-        options={{ headerShown: false }}
-      />
-      
-      <Stack.Screen
-        name="(tabs)"
-        options={{ headerShown: false }}
-      />
-    </Stack>
+    <TransactionProvider>
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{ headerShown: false }}
+        />
+        
+        <Stack.Screen
+          name="login"
+          options={{ headerShown: false }}
+          />
+        
+        <Stack.Screen
+          name="(tabs)"
+          options={{ headerShown: false }}
+          />
+      </Stack>
+    </TransactionProvider>
   );
 }
