b1863de039b12db818a17e165b29abc52da0d076
RPH07 <renaldyputrahasli@gmail.com>
2026-01-28 14:35:09 +0700

    fix: update API URL and correct content type and authorization header

--- mobile-app/services/api.ts
+++ mobile-app/services/api.ts
@@ -1,35 +1,29 @@
 import axios from 'axios';
 import * as SecureStore from 'expo-secure-store';
 
-const BASE_URL = 'http://192.168.1.10:3000/api';
+const API_URL = 'http://192.168.1.9:5000/api'; 
 
 const api = axios.create({
-    baseURL: BASE_URL,
+    baseURL: API_URL,
     headers: {
-        "Content-Type": 'aplication/json',
+        'Content-Type': 'application/json',
     },
     timeout: 10000,
 });
 
 api.interceptors.request.use(
-    async(config) => {
-        const token = await SecureStore.getItemAsync('userToken');
-        if (token) {
-            config.headers.Authorization = `Beare ${token}`;
+    async (config) => {
+        try {
+            const token = await SecureStore.getItemAsync('userToken');
+            if (token) {
+                config.headers.Authorization = `Bearer ${token}`;
+            }
+        } catch (error) {
+            console.error("Gagal ambil token:", error);
         }
         return config;
     },
     (error) => {
-        return Promise.reject(error);
-    }
-);
-
-api.interceptors.response.use(
-    (response) => response,
-    (error) => {
-        if (error.response && error.response.status === 401) {
-            console.log("Unauthorize! Token Invalid!")
-        }
         return Promise.reject(error);
     }
 );
